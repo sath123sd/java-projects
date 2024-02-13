fba7ed957c25a9d2db0abbb68f56c9c6e28f89be
tsath <tsathvika22@gmail.com>
2024-02-13 12:53:05 -0600

    Seventh commit

---
+++ src/SumCalculator.java
@@ -0,0 +1,12 @@
+interface SumCalculator {
+    int sum(int a, int b);
+}
+class Sum {
+    public static void main(String[] args) {
+        SumCalculator sumCalculator = (x, y) -> x + y;
+        int result = sumCalculator.sum(7, 6);
+        System.out.println("Sum 7,6): " + result);
+        result = sumCalculator.sum(15, -35);
+        System.out.println("Sum 15, -35): " + result);
+    }
+}
